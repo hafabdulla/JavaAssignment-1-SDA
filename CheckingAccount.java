86259601090fe38d1001f26e0ff07fd816af5f94
Hafiz Abdullah <abdullakhaliq22@gmail.com>
2025-09-07 18:39:42 +0500

    Add files via upload

---
+++ CheckingAccount.java
@@ -0,0 +1,69 @@
+
+public class CheckingAccount extends AbstractAccount {
+	public static final int FreeTransactions = 2;
+	public static final double TransactionFee = 10.0;
+	
+	private int monthlyTransactionCount = 0;
+	private double totalFeesDeducted = 0.0;
+	
+//Constructor
+	public CheckingAccount(String accNumber, double opBalance) {
+		super(accNumber, opBalance);
+	}
+		
+@Override
+	public boolean makeWithdrawal(double amount) {
+		if(amount <= 0) {
+			System.out.println("Withdrawal amount is not correct");
+			return false;
+		}
+        // overdraft allowed up to -5000
+		if(this.getBalance() - amount < -5000) {
+			System.out.println("Insufficient balance, overdraft limit exceeded");
+			return false;
+		}
+		debit(amount, "Withdrawal");
+		applyTransactionFee("WithdrawalFee");
+		System.out.println("Withdrawal of " + amount + " successful");	
+		return true;
+	}
+
+@Override
+	public boolean makeDeposit(double amount) {
+		if(amount <= 0)	 {
+			System.out.println("Deposit amount is not correct");
+			return false;
+		}
+		credit(amount, "Deposit");
+		applyTransactionFee("DepositFee");
+		System.out.println("Deposit of " + amount + " successful");
+		return true;
+	}
+
+@Override
+	public double calculateZakat() {
+		// No zakat for checking accounts
+		return 0.0;
+	}
+
+@Override
+	public void displayDeductions() {
+		System.out.println("---- Deductions for Checking Account ----");
+		System.out.println("Total Transaction Fees Deducted: " + formatMoney(totalFeesDeducted));	
+		System.out.println("-----------------------------------------");
+	}
+
+@Override
+	public String getAccountType() {
+		return "CHECKING";
+	}
+
+	private void applyTransactionFee(String feeType) {
+		monthlyTransactionCount++;
+		if(monthlyTransactionCount > FreeTransactions) {
+			logFee(TransactionFee, feeType);
+			totalFeesDeducted += TransactionFee;
+			System.out.println("Transaction fee of " + TransactionFee + " applied for " + feeType);
+		}
+	}
+}
---
+++ SavingsAccount.java
@@ -0,0 +1,70 @@
+
+public class SavingsAccount extends AbstractAccount {
+	private static double interestRate = 0.02; //2% annual interest rate
+	private double totalZakatDeduction = 0.0;
+	
+//Constructor
+	public SavingsAccount(String accNumber, double opBalance) {
+		super(accNumber, opBalance);
+	}
+	
+@Override
+	public boolean makeWithdrawal(double amount) {
+		if(amount <= 0) {
+			System.out.println("Withdrawal amount is not correct");
+			return false;
+		}
+		if(amount > this.getBalance()) {
+			System.out.println("Insufficient balance");
+			return false;
+		}
+		debit(amount, "Withdrawal");
+		System.out.println("Withdrawal of " + amount + " successful");
+		return true;
+	}
+
+@Override
+	public double calculateZakat() {
+	double balance = this.getBalance();
+
+		if(balance >= 20000) {
+			double zakat = balance * 0.025; //2.5% of balance
+			debit(zakat, "Zakat Deduction");
+			totalZakatDeduction += zakat;
+			System.out.println("Zakat of " + zakat + " deducted from account " + this.getAccountNumber());
+			return zakat;
+		}
+	return 0.0; // no zakat if balance < 20,000
+	}
+
+@Override
+	public void displayDeductions() {
+    	System.out.println("---- Deductions for Savings Account ----");
+    	System.out.println("Total Zakat Deducted: " + formatMoney(totalZakatDeduction));	
+    	System.out.println("----------------------------------------");
+	}
+@Override
+	public String getAccountType() {
+		return "SVINGS";
+	}	
+
+	public void calculateInterest() {
+		double balance = this.getBalance();
+		double interest = balance * (interestRate); //monthly interest
+		credit(interest, "Interest");
+		System.out.println("Interest of " + interest + " credited to account " + this.getAccountNumber());
+	}
+	
+	public static void setInterestRate(double newRate) {
+		if(newRate < 0) {
+			System.out.println("Interest rate cannot be negative");
+			return ;
+		}
+		interestRate = newRate;
+		System.out.println("Interest rate set to " + (newRate * 100) + "%");
+	}
+	public static double getInterestRate() {
+		return interestRate;
+	}
+	
+}
